52886dd1529d1fbe1f9ebc35a6f59475863e9c64
Borirat <Boriratkk@gmail.com>
2026-01-11 23:22:22 +0700

    test: force playwright to fails 5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('force fail demo', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  expect(1).toBe(2); // ❌ fail แน่นอน
+  expect(1).toBe(2); // ❌ fail แน่นอน 555
 });

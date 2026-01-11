eec581f25fa17b1fe420232e24e5c2b9cde76b2e
Borirat <Boriratkk@gmail.com>
2026-01-11 22:26:45 +0700

    test: force playwright fail

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('force fail demo', async ({ page }) => {
+test('FAIL DEMO', async ({ page }) => {
   await page.goto('http://localhost:3000');
-  await expect(page).toHaveTitle('THIS TITLE MUST FAIL');
+  await expect(page).toHaveTitle('THIS TITLE WILL NEVER MATCH');
 });

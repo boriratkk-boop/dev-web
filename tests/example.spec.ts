946b1f9ad1c6e1bf5f767476ea8f6bee7bce56d7
Borirat <Boriratkk@gmail.com>
2026-01-11 22:18:51 +0700

    test: force playwright to fail

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-  expect(1).toBe(2);
+test('force fail demo', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  await expect(page).toHaveTitle('THIS TITLE MUST FAIL');
 });
